555c69749b6f3ae59074dde657279970bf85c916
Team9019 <oleniczak@ak.net>
2017-03-21 23:01:13 -0800

    Work with menus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_Distance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_Distance.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  * Created by Aimee on 3/2/2017.
  */
 
-@Autonomous(name="TEST - Function", group="Test")
+@Autonomous(name="TEST - Function", group="Utilities")
 //@Disabled
 public class TEST_Distance extends LinearOpMode
 {
@@ -16,11 +16,15 @@ public class TEST_Distance extends LinearOpMode
     private Commands cmds = new Commands(telemetry, this);
     private Initialize init = new Initialize(telemetry);
 
+    P_Menu_Commands menu =new P_Menu_Commands();
+
     private int TimeDebugSleep = 1500;
 
     @Override
     public void runOpMode() throws InterruptedException
     {
+        menu.init(hardwareMap.appContext,this);
+
         telemetry.addData("BEGIN", "Autonomous Starting...");
         telemetry.update();
 
@@ -30,8 +34,21 @@ public class TEST_Distance extends LinearOpMode
 
         init.InitializeHW(robot);
 
+        //if Config Menu has been run, use it's value.  If not, program will continue to use configproperties value
+//        if (menu.param.colorAlliance != null)
+//        {
+//            telemetry.addData("Config", "Menu:  " + menu.param.colorAlliance + " Alliance");
+            Configuration.ALLIANCE = (String) menu.param.colorAlliance.name();
+//        }
         telemetry.addData("Config", Configuration.ALLIANCE + " Alliance");
+
+//        if (menu.param.autonType != null)
+//        {
+//            telemetry.addData("Config", "Menu:  " + menu.param.autonType + " Position");
+            Configuration.START_POSITION =  (String) menu.param.autonType.name();
+//        }
         telemetry.addData("Config", Configuration.START_POSITION + " Starting Position");
+
         telemetry.addData("Config", Configuration.AUTO_DELAY /1000 + " Sec. Delay");
         telemetry.addData("Config","Initialization Complete!");
         telemetry.update();
